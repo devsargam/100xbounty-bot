53f7cff7fe5a9db1976dfcc8ddf7682750847693
devsargam <sargampoudel100@gmail.com>
2024-03-16 01:52:30 +0530

    fix: bot now replies to events

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import { Probot } from "probot";
 
 export default (app: Probot) => {
+  app.log.info("Yay! The app was loaded!");
+
   app.on("issues.opened", async (context) => {
+    app.log.info("An issue was edited");
     const issueComment = context.issue({
       body: "Thanks for opening this issue!",
     });

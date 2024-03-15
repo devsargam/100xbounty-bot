82b6510b1ddb138e5429fe2dd5ac88933b555573
devsargam <sargampoudel100@gmail.com>
2024-03-16 00:20:02 +0530

    checkpoint: new framework

--- src/discord/webhook.ts
+++
@@ -1,5 +0,0 @@
-import { WebhookClient } from "discord.js";
-
-export const discordWebhookClient = new WebhookClient({
-  url: process.env.DISCORD_WEBHOOK_URL!,
-});
--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,15 @@
-import { createEventHandler } from "@octokit/webhooks";
-import "dotenv/config";
-import EventSource from "eventsource";
-import { commentHandler } from "./handlers";
-import { EventCache } from "./cache";
-import { issueCommentSchema } from "./validator";
+import { Probot } from "probot";
 
-const webhookProxyUrl = "https://smee.io/BjvGNtdloGcCODsW";
-
-const cache = new EventCache();
-const source = new EventSource(webhookProxyUrl);
-
-source.onmessage = (event) => {
-  const webhookEvent = JSON.parse(event.data);
-
-  const eventHandler = createEventHandler({
-    async transform(event) {
-      return event;
-    },
+export default (app: Probot) => {
+  app.on("issues.opened", async (context) => {
+    const issueComment = context.issue({
+      body: "Thanks for opening this issue!",
+    });
+    await context.octokit.issues.createComment(issueComment);
   });
+  // For more information on building apps:
+  // https://probot.github.io/docs/
 
-  eventHandler.on("issue_comment.created", async ({ id, payload }) => {
-    // TODO: Fix this somehow I am getting 2 messages for same event
-    if (cache.idPresent(id)) return;
-    cache.setId(id);
-    console.log(payload);
-
-    const result = issueCommentSchema.safeParse(payload);
-    if (!result.success) return console.log(result.error);
-
-    commentHandler({ id, payload: result.data });
-  });
-
-  eventHandler
-    .receive({
-      id: webhookEvent["x-github-delivery"],
-      name: webhookEvent["x-github-event"],
-      payload: webhookEvent.body,
-    })
-    .catch();
+  // To get your app running against GitHub, see:
+  // https://probot.github.io/docs/development/
 };

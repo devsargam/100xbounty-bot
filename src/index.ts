9a2f1f28a1af8e5078a8a77c6ef33489b7d87435
devsargam <sargampoudel100@gmail.com>
2024-03-16 01:59:41 +0530

    fix: setup events `issues`, `pr`, `comments`

--- src/index.ts
+++ src/index.ts
@@ -4,15 +4,31 @@ export default (app: Probot) => {
   app.log.info("Yay! The app was loaded!");
 
   app.on("issues.opened", async (context) => {
+    if(context.isBot) return;
     app.log.info("An issue was edited");
     const issueComment = context.issue({
       body: "Thanks for opening this issue!",
     });
     await context.octokit.issues.createComment(issueComment);
   });
-  // For more information on building apps:
-  // https://probot.github.io/docs/
 
-  // To get your app running against GitHub, see:
-  // https://probot.github.io/docs/development/
+  app.on("pull_request.opened", async (context) => {
+    if(context.isBot) return;
+    // TODO: Handle case when pr is opened
+    app.log.info("An pr was created");
+    const issueComment = context.issue({
+      body: "Thanks for opening this PR!",
+    });
+    await context.octokit.issues.createComment(issueComment);
+  })
+
+  app.on("issue_comment.created", async(context) => {
+    if(context.isBot) return;
+    // TODO: Handle case when comment is created
+    app.log.info("A comment was created");
+    const issueComment = context.issue({
+      body: "Thanks for creating a comment!",
+    });
+    await context.octokit.issues.createComment(issueComment);
+  })
 };
